61cacb04225f95ad9b275056df52bb3d7725e65f
Niro <Niro@gmail.com>
2022-01-28 04:18:09 -0800

    firstCommit

---
+++ src/test/java/DemoProject/DemoProject/FirstPgChromeTest.java
@@ -0,0 +1,22 @@
+package DemoProject.DemoProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstPgChromeTest {
+	
+	public static void main(String args[]) {
+		
+		System.setProperty("webdriver.chrome.driver" ,"C:\\Users\\hp\\eclipse-workspace\\DemoProject\\Drivers\\chromedriver.exe" );
+	   WebDriver driver=new ChromeDriver();
+	   driver.manage().deleteAllCookies();
+	   driver.manage().window().maximize();
+	   driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
+	   driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+	   driver.get("https://www.google.co.in/");
+	   
+	}
+
+}
